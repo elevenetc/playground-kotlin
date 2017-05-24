f6c03bc60ff90a9f4efb333ed36588f4d24d0b27
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-05-25 00:22:02 +0200

    Add sample node and field

---
+++ src/test/Tests.kt
@@ -0,0 +1,15 @@
+package test
+
+import main.Node
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Created by eugene.levenetc on 24/05/2017.
+ */
+
+class Tests {
+    @Test fun testZ() {
+        val node = Node(1)
+    }
+}
